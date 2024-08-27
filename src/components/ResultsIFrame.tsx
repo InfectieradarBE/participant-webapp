87ac2479824fb4da3cfa6ea608e57a4ee52281d0
roelsprimontuhasselt <roel.sprimont@uhasselt.be>
2024-08-27 13:27:21 +0200

    Add IFrame Resizer and add dashboard.infectieradar.be as URL

--- src/components/ResultsIFrame.tsx
+++ src/components/ResultsIFrame.tsx
@@ -1,16 +1,23 @@
 import { useTranslation } from "react-i18next";
+import IframeResizer  from "@iframe-resizer/react";
 
 const ResultsIFrame = (props: any) => {
     const { i18n } = useTranslation();
 
-    const baseUrl = "http://127.0.0.1:5432/"
+    const baseUrl = "http://dashboard.infectieradar.be/"
     const iframeURL = `${baseUrl}?lang=${i18n.language}`
 
     return (
-        <iframe
-            style={{ width: "100%", height: "1800px" }}
-            title="Results IFrame"
+        //<iframe
+        //    style={{ width: "100%", height: "1800px" }}
+        //    title="Results IFrame"
+        //    src={iframeURL}
+        ///>
+        <IframeResizer
+            id="iframe-target"
+            license="GPLv3" // development only, license key required for production
             src={iframeURL}
+            style={{ width: '100%',  height: '100vh' }}
         />
     );
 };
